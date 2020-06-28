9563fbc0889f74461efc9b5c50f6d8bacce194d3
GeovaniiSilva <geapsi@gmail.com>
2020-06-28 11:32:32 -0300

    UPDATE: adjusts template

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def edit(todo_id):
     return redirect(url_for("home"))
 
 
-@app.route("/delete/<int:todo_id>/")
+@app.route("/delete/<int:todo_id>/", methods=["POST"])
 def delete(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
